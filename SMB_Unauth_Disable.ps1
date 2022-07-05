3cc527367b2d815c2a6c73b6a294332bd1d42187
LewisB321 <107862880+LewisB321@users.noreply.github.com>
2022-07-05 12:13:29 +0100

    Update SMB_Unauth_Disable.ps1

--- SMB_Unauth_Disable.ps1
+++ SMB_Unauth_Disable.ps1
@@ -1,2 +1,4 @@
+#Disable SMB anonymous access
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\LSA" RestrictAnonymous -Type DWORD -Value 1 -Force
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\LSA" RestrictAnonymoussam -Type DWORD -Value 1 -Force
+Restart-Computer -False
