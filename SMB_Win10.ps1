12efaee1544358dff921e24f44766b0474d09dae
LewisB321 <107862880+LewisB321@users.noreply.github.com>
2022-07-05 11:30:38 +0100

    Update SMB_Win10.ps1

--- SMB_Win10.ps1
+++ SMB_Win10.ps1
@@ -1,3 +1,3 @@
 #Disable SMB1 on Windows client
-Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart #Method of bypassing the restart prompt
 Restart-Computer -Force
