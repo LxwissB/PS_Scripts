c1c1ef27aff299f2d2e7e03edce109895a5555e9
LewisB321 <107862880+LewisB321@users.noreply.github.com>
2022-07-05 12:04:02 +0100

    Update SMB_WinServ.ps1

--- SMB_WinServ.ps1
+++ SMB_WinServ.ps1
@@ -1,4 +1,5 @@
-#Disable SMB1 on Windows server
-Set-SmbServerConfiguration -EnableSMB1Protocol $false
-Set-SmbServerConfiguration -EnableSMB2Protocol $true
+#Disable SMB1 on Windows server and enable others (if disabled)
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+Set-SmbServerConfiguration -EnableSMB2Protocol $true -Force
+Set-SmbServerConfiguration -EnableSMB3Protocol $true -Force
 Restart-Computer -Force
