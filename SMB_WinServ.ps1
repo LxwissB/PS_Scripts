8098b7386b8b2365b1b4662b32dc90f5c954837a
LewisB321 <107862880+LewisB321@users.noreply.github.com>
2022-07-05 12:07:19 +0100

    Update SMB_WinServ.ps1

--- SMB_WinServ.ps1
+++ SMB_WinServ.ps1
@@ -2,4 +2,3 @@
 Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
 Set-SmbServerConfiguration -EnableSMB2Protocol $true -Force
 Set-SmbServerConfiguration -EnableSMB3Protocol $true -Force
-Restart-Computer -Force
