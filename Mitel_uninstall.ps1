a6dee17c7081828813bab9ca4981519bc25f4852
LewisB321 <107862880+LewisB321@users.noreply.github.com>
2022-08-22 12:40:24 +0100

    Add files via upload

---
+++ Mitel_uninstall.ps1
@@ -0,0 +1,3 @@
+﻿#Uninstall script for Silverlight
+$application = Get-WmiObject -Class Win32_Product -Filter "IdentifyingNumber = '{E0B09664-5C46-42F7-B5D4-808F7DC79EF8}'"
+$application.Uninstall()
---
+++ Silverlight_uninstall.ps1
@@ -0,0 +1,3 @@
+﻿#Uninstall script for Silverlight
+$application = Get-WmiObject -Class Win32_Product -Filter "IdentifyingNumber = '{89F4137D-6C26-4A84-BDB8-2E5A4BB71E00}'"
+$application.Uninstall()
